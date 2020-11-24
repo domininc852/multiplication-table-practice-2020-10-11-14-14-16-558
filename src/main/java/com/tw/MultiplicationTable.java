e3e4de48fb6400fb99ae555774ecc82a63244c79
NSC/Network Service Center <NSC@oocl.com>
2020-11-24 18:59:06 +0800

    [MultiplicationTable][Dominic Leung] feat: Implement the whole logic for generating multiplication table

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,7 +1,42 @@
 package com.tw;
 
 public class MultiplicationTable {
-    public String create(int startNumber, int endNumber) {
-        return null;
+    public String generateMultiplicationTableWithRange(int start, int end) {
+        boolean isStartSmallerThanEnd = isStartSmallerThanEnd(start, end);
+        boolean isStartAndEndValid = isStartAndEndValid(start, end);
+        return isStartSmallerThanEnd && isStartAndEndValid?generateMultiplicationTable(start, end):null;
+
     }
+
+    private boolean isStartSmallerThanEnd(int start, int end) {
+        return  end >= start;
+    }
+
+    private boolean isStartAndEndValid(int start, int end) {
+        return (start >= 1) && (start <= 1000) && (end >= 1) && (end <= 1000);
+
+    }
+
+    private String generateMultiplicationTable(int start, int end) {
+        String multiplicationTable = "";
+        for (int i = start; i <= end; i++) {
+            multiplicationTable += generateMultiplicationLine(start, i);
+            if (i != end) {
+                multiplicationTable += "\r\n";
+            }
+        }
+        return multiplicationTable;
+    }
+
+    private String generateMultiplicationLine(int start, int end) {
+        String line = "";
+        for (int i = start; i <= end; i++) {
+            line += i + "*" + end + "=" + i * end;
+            if (i != end) {
+                line += "  ";
+            }
+        }
+        return line;
+    }
+
 }
